cd4def2f67792aff67530bec58c6e60d33554a8e
Vova640 <volodimirsinkaruk513@gmail.com>
2025-12-06 20:39:11 +0200

    Delete рік народження.js

--- рік народження.js
+++
@@ -1,8 +0,0 @@
-const person = { name: 'John', age: 23, city: 'Boston' };
-
-Object.defineProperty(person, 'birthYear', {
-  get() { return new Date().getFullYear() - this.age; },
-  enumerable: true
-});
-
-console.log(person.birthYear);
